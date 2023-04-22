94328ebe3e2e5d8def6552944791424ecfb58d4c
Leonid Medovyy <leonid@storypro.io>
2023-04-21 19:49:50 -0700

    adding pinecone

--- pages/api/dataset/answer.tsx
+++ pages/api/dataset/answer.tsx
@@ -1,13 +1,59 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {supabase} from "../../../lib/client";
 
+import { VectorDBQAChain } from "langchain/chains";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { OpenAI } from "langchain/llms/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { pinecone } from '../../../lib/pinecone_client';
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '../../../lib/pinecone_settings';
+
+
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
     const setId = req.body['setId']
+
+    let text= ""
 
     const { data, error } = await supabase
         .from('dataset')
         .select('text')
         .eq('set_id', setId)
 
-    res.status(200).json({data: data, error: error})
+
+    for (let i = 0; i < data!.length; i++) {
+        text += data![i].text + " "
+    }
+
+
+    const model = new OpenAI({ maxTokens: 1000, temperature: 0.1 });
+
+    const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+    const docs = await textSplitter.createDocuments([text]);
+
+    console.log(docs)
+
+    const embeddings = new OpenAIEmbeddings();
+    const index = pinecone.Index(PINECONE_INDEX_NAME);
+
+    await PineconeStore.fromDocuments(docs, embeddings, {
+        pineconeIndex: index,
+        namespace: PINECONE_NAME_SPACE,
+        textKey: 'text',
+    });
+
+    // const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+    // const qaChain = VectorDBQAChain.fromLLM(model, vectorStore);
+
+    // const answer = await qaChain.call({
+    //     input_documents: docs,
+    //     query: "You are a reporter for New York Times. " + 'Can you summarize the article?',
+    // });
+
+    const answer = {}
+
+    res.status(200).json({answer: answer })
 }
