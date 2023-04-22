e8b13a764bef729e1b9a7619e993a9ef66a41bcc
Leonid Medovyy <leonid@storypro.io>
2023-04-21 17:56:50 -0700

    setting up the base for q-n-a

--- pages/api/dataset/answer.tsx
+++ pages/api/dataset/answer.tsx
@@ -1,5 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {supabase} from "../../../lib/client";
 
-export default function handler(req:NextApiRequest, res:NextApiResponse) {
-    res.status(200).json({test: 'answering'})
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    const setId = req.body['setId']
+
+    const { data, error } = await supabase
+        .from('dataset')
+        .select('text')
+        .eq('set_id', setId)
+
+    res.status(200).json({data: data, error: error})
 }
