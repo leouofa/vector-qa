531968d6473e06d1d5cdba06836a928dcc3ca80d
Leonid Medovyy <leonid@storypro.io>
2023-04-21 17:47:23 -0700

    setting up the train api

--- pages/api/dataset/train.tsx
+++ pages/api/dataset/train.tsx
@@ -11,5 +11,5 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
         .insert({ set_id: setId, text: text })
         .select()
 
-    res.status(200).json(data)
+    res.status(200).json({data: data, error: error})
 }
